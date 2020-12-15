413e0ed9779ecbb483a3de460d7601b1e4f9ae99
NYECK VICTOR <minyemmdm@gmail.com>
2020-12-15 09:33:27 +0100

    Implementation des fichier log et connection a la base de donnee

---
+++ ControleurRestaurant/FileLog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleurRestaurant
+{
+    class FileLog
+    {
+        public void ajouterLog(string filePath)
+        {
+            using (StreamWriter w = File.AppendText(filePath+".txt"))
+            {
+                Log("Test1", w);
+            }
+
+            /*using (StreamReader r = File.OpenText("log.txt"))
+            {
+                DumpLog(r);
+            }*/
+        }
+
+        public static void Log(string logMessage, TextWriter w)
+        {
+            w.Write("\r\nLog Entry : ");
+            w.WriteLine($"{DateTime.Now.ToLongTimeString()} {DateTime.Now.ToLongDateString()}");
+            w.WriteLine("  :");
+            w.WriteLine($"  :{logMessage}");
+            w.WriteLine("-------------------------------");
+        }
+
+        public static void DumpLog(StreamReader r)
+        {
+            string line;
+            while ((line = r.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ RestaurantSimulationProject/Restaurant/ToolsRestaurant/EnCuisine/RecetteDeCuisine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.ToolsRestaurant.EnCuisine
+{
+    class RecetteDeCuisine
+    {
+        // attribut des recettes de cuisine
+        private string name;
+        private Dictionary<IngredientEnCuisine, int> ListOfIngredient;
+        private Dictionary<MaterielsDeCuisine, int> ListeOfMaterielsForCook;
+    }
+}
