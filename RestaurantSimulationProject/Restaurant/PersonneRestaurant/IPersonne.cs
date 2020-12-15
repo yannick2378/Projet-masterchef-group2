6ad96466d619df6aa1e72844e510c21a6b13962a
NYECK VICTOR <minyemmdm@gmail.com>
2020-12-15 07:17:09 +0100

    la premiere version des classes models du restaurant notamment la cuisine

---
+++ RestaurantSimulationProject/Restaurant/PersonneRestaurant/IPersonne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public enum ZoneTravail
+    {
+        Kitchen,
+        DiningRoom
+    }
+    public enum Horaire
+    {
+        ServiceDuMatin,
+        ServiceDuSoir
+    }
+    public enum StatusDuTravail
+    {
+        NotWork,
+        WorkRunning,
+        WorkFinished
+    }
+    public interface IPersonne
+    {
+        string name { get; set; }
+        string surname { get; set; }
+        int idPersonne { get; set; }
+        // methode de l'interface
+        void creerPersonne(string name, string surname);
+        void autoId();
+    }
+}
---
+++ RestaurantSimulationProject/Restaurant/PersonneRestaurant/employes/EmployesEnCuisine.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Restaurant.ToolsRestaurant.EnCuisine;
+
+namespace Restaurant.PersonneRestaurant.employes
+{
+    public class EmployesEnCuisine : EmployesRestaurant
+    {
+        // les attributs propre a un employe en cuisine
+        protected Dictionary<MaterielsDeCuisine,int> listOfMateriels;
+        protected Dictionary<IngredientEnCuisine, int> listOfIngredients;
+
+        // methodes qui permettent a un employe de cuisine d'interagir avec ses outils
+        public EmployesEnCuisine()
+        {
+            listOfMateriels = null;
+        }
+        
+        public void initialiserEmploye(Dictionary<MaterielsDeCuisine,int> listOfTools = null,Dictionary<IngredientEnCuisine,int> listofIngredients = null)
+        {
+            if(listOfTools != null)
+            {
+                this.listOfMateriels = listOfTools;
+            }
+            if(listofIngredients != null)
+            {
+                this.listOfIngredients = listofIngredients;
+            }
+        }
+        public StatusDuTravail utilisationTools(int tempsOfWorking)
+        {
+            Dictionary<MaterielsDeCuisine,int>.KeyCollection myKeyCollection = listOfMateriels.Keys;// On recuperejuste les clefs
+            Dictionary<IngredientEnCuisine, int>.KeyCollection mykeyCollection2 = listOfIngredients.Keys;
+
+            this.statutDuTravail = StatusDuTravail.WorkRunning;
+            if(this.listOfMateriels == null && this.listOfIngredients == null)
+            {
+                return this.statutDuTravail = StatusDuTravail.NotWork;
+            }
+            else
+            {
+                if(this.listOfIngredients != null)
+                {
+                    foreach(KeyValuePair<IngredientEnCuisine,int> kvp in listOfIngredients)
+                    {
+                        kvp.Key.ingredientIsUsing(kvp.Value);
+                    }
+                }
+
+                if (this.listOfMateriels != null)
+                {
+                    foreach (KeyValuePair<MaterielsDeCuisine, int> kvp in listOfMateriels)
+                    {
+                        kvp.Key.materielIsUsing(kvp.Value);
+                    }
+                }
+            }
+            return this.statutDuTravail = StatusDuTravail.WorkFinished;
+        }
+
+    }
+}
