3590b9c73a82990d555e6fadec81383dde41195f
NYECK VICTOR <minyemmdm@gmail.com>
2020-12-16 03:49:28 +0100

    implementaton des classe chef et du controleur

---
+++ RestaurantSimulationProject/Restaurant/ToolsRestaurant/Commande/CommandePlat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.ToolsRestaurant.Commande
+{
+    class CommandePlat
+    {
+        private int numeroDeTable;
+        private List<int> entreeRecette;
+        private List<int> resistanceRecette;
+        private List<int> dessertRecette;
+
+        // methode la classe commande 
+        public CommandePlat()
+        {
+            numeroDeTable = 0;
+            entreeRecette = null;
+            resistanceRecette = null;
+            dessertRecette = null;
+        }
+        
+        public void prendreUneCommande(int nDT,List<int> eR, List<int> rR, List<int> dR)
+        {
+            numeroDeTable = nDT;
+            entreeRecette = eR;
+            resistanceRecette = rR;
+            dessertRecette = dR;
+        }
+    }
+}
